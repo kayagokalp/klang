5a836f4d0b4ebf0781e3e07bebd1bf6c7522d07c
Kaya Gokalp <kaya.gokalp@fuel.sh>
2023-02-26 18:41:30 +0300

    chore: fmt

--- klang-parse/src/lexer.rs
+++ klang-parse/src/lexer.rs
@@ -46,12 +46,11 @@ pub fn tokenize(input: &str) -> anyhow::Result<TokenStream> {
             Token::ClosingParenthesis
         } else if cap.name("comma").is_some() {
             Token::Comma
-        } else if cap.name("opbrace").is_some(){
+        } else if cap.name("opbrace").is_some() {
             Token::OpeningBrace
-        } else if cap.name("clbrace").is_some(){
+        } else if cap.name("clbrace").is_some() {
             Token::ClosingBrace
-        }
-        else{
+        } else {
             let operator = cap
                 .name("operator")
                 .ok_or_else(|| anyhow::anyhow!("lexer failed trying to get operator"))?;
@@ -66,8 +65,8 @@ pub fn tokenize(input: &str) -> anyhow::Result<TokenStream> {
 
 #[cfg(test)]
 mod test {
-    use crate::token::Token;
     use super::tokenize;
+    use crate::token::Token;
 
     #[test]
     fn test_lex_pub_keyword() {
@@ -89,7 +88,12 @@ mod test {
     fn test_lex_ident() {
         let input_str = r#"this is a ident"#;
         let token_stream = tokenize(input_str).unwrap();
-        let expected = vec![Token::Ident("this".to_string()), Token::Ident("is".to_string()), Token::Ident("a".to_string()), Token::Ident("ident".to_string())];
+        let expected = vec![
+            Token::Ident("this".to_string()),
+            Token::Ident("is".to_string()),
+            Token::Ident("a".to_string()),
+            Token::Ident("ident".to_string()),
+        ];
         assert_eq!(token_stream, expected)
     }
 
@@ -113,7 +117,14 @@ mod test {
     fn test_lex_fun_decl() {
         let input_str = r#"fun this_is_a_decl() {}"#;
         let token_stream = tokenize(input_str).unwrap();
-        let expected = vec![Token::Fun, Token::Ident("this_is_a_decl".to_string()), Token::OpeningParenthesis, Token::ClosingParenthesis, Token::OpeningBrace, Token::ClosingBrace];
+        let expected = vec![
+            Token::Fun,
+            Token::Ident("this_is_a_decl".to_string()),
+            Token::OpeningParenthesis,
+            Token::ClosingParenthesis,
+            Token::OpeningBrace,
+            Token::ClosingBrace,
+        ];
         assert_eq!(token_stream, expected)
     }
 
@@ -124,5 +135,4 @@ mod test {
         let expected = vec![Token::Number(102.0)];
         assert_eq!(token_stream, expected)
     }
-
 }
