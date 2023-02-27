3ab51d8fca1e23b100946feec8a52df7d5fb499d
Kaya Gokalp <kaya.gokalp@fuel.sh>
2023-02-27 22:45:27 +0300

    context implemented

--- klang-core/src/lib.rs
+++ klang-core/src/lib.rs
@@ -1,4 +1,8 @@
-use anyhow::Result;
+mod context;
+mod ir_builder;
+mod module;
+
+use anyhow::{Ok, Result};
 use klang_ast::node::ASTNode;
 use klang_parse::{lexer::tokenize, parser::parse, token::Token};
 
@@ -9,4 +13,9 @@ pub fn parse_to_ast(input_str: &str) -> ParseResult {
     let token_stream = tokenize(input_str)?;
     let parsed_nodes = vec![];
     parse(&token_stream, &parsed_nodes).map_err(|e| anyhow::anyhow!("{e}"))
+}
+
+/// Convert the given AST to llvm-ir.
+pub fn ast_to_ir() -> Result<()> {
+    Ok(())
 }
